d1bd74838e7229920f6029a698a289bdb17363b9
Imtiaz Hossain <imtiaz15-11790@diu.edu.bd>
2022-08-22 01:04:18 +0600

    Practice Date 21.8.22 book practice till page 113.

---
+++ src/books/O1_head_first_java/chapter_01/O4_Random/PhraseOMatic.java
@@ -0,0 +1,36 @@
+package books.O1_head_first_java.chapter_01.O4_Random;
+
+import java.util.Random;
+
+public class PhraseOMatic {
+    public static void main(String[] args) {
+
+        // make three sets of words to choose from. Add your own!
+        String[] wordListOne = {"Md Imtiaz", "A N M Bajlur rahman", "Sohag", "Seikh"};
+
+        String[] wordListTwo = {"House", "Bilding", "Room", "Dining"};
+
+        String[] wordListThree = {"Chair", "Table", "Door"};
+
+        // find out how many words are in each list
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+
+        // generate three random numbers
+        Random randomGenerator = new java.util.Random();
+
+        int rand1 = randomGenerator.nextInt(oneLength);
+        int rand2 = randomGenerator.nextInt(twoLength);
+        int rand3 = randomGenerator.nextInt(threeLength);
+
+        // now build a phrase
+        String phrase = wordListOne[rand1] + "'s " + wordListTwo[rand2] + " " + wordListThree[rand3];
+
+        // print out the phrase
+        System.out.println("What we need is a " + phrase);
+    }
+}
+
+
+// Book: Head First Java, Page: 108 / 1597
---
+++ src/books/O1_head_first_java/chapter_01/O5_Practice/Main.java
@@ -0,0 +1,30 @@
+package books.O1_head_first_java.chapter_01.O5_Practice;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 3;
+
+        while (x > 0) {
+            if (x > 2) {
+                System.out.print("a");
+            }
+            x = x - 1;
+            System.out.print("-");
+            if (x == 2) {
+                System.out.print("b c");
+            }
+            x = x - 1;
+            System.out.print("-");
+            if (x == 1) {
+                System.out.print("d");
+                x = x - 1;
+            }
+        }
+    }
+}
+
+
+// Task Output: a-b c-d
+
+// Book: Head First Java, Page: 112 / 1597
